5e78ed1498c11f0ae2f263d261007e4711b425b9
MaramMaryam <50516756+MaramMaryam@users.noreply.github.com>
2020-05-19 11:10:14 +0430

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, {useContext, useReducer} from 'react';
+import './App.css';
+import Nav from "./components/Nav";
+//import NotesContext from "./context/Context";
+//import notesReducer from "./context/reducer";
+
+function App() {
+    //const initialState = useContext(NotesContext);
+    //const [state,dispatch] = useReducer(notesReducer,initialState);
+    return (
+        <><Nav/>
+
+{/*            <NotesContext.Provider>
+                <Nav/>
+            </NotesContext.Provider>*/}
+
+        </>
+    );
+
+
+}
+
+export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Nav(props) {
+    return (
+        <div>
+            <nav> ToDos App </nav>
+        </div>
+    );
+}
+
+export default Nav;
