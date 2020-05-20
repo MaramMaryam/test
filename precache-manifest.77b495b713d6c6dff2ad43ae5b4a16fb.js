1b6ad70ef9e82078d6e7fc8d8d260dded3490658
Maryam <dev.m.danehvash2014@gmail.com>
2020-05-20 06:22:07 +0430

    Updates

---
+++ precache-manifest.77b495b713d6c6dff2ad43ae5b4a16fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e3134e7657a0cd5f5a0f998267f1919",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "d39ec4f637681567b0d2",
+    "url": "/test/static/css/main.e25c3694.chunk.css"
+  },
+  {
+    "revision": "32091f55d70ad9369c0f",
+    "url": "/test/static/js/2.69adc4a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.69adc4a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d39ec4f637681567b0d2",
+    "url": "/test/static/js/main.e4d5add2.chunk.js"
+  },
+  {
+    "revision": "d3028e0adeb2a6ef90b0",
+    "url": "/test/static/js/runtime-main.fec0f9ce.js"
+  }
+]);
